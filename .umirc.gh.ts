06c78716cf963e8c84bdfbfafc95d6ff528ab13c
Sushi Su <s.xiuxing@foxmail.com>
2022-06-12 16:21:11 +0800

    ci(gh-pages): publicPath

---
+++ .umirc.gh.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from '@umijs/max';
+
+export default defineConfig({
+  define: {
+    TOKEN_KEY: 'RUOYI_TOKEN',
+    API_HOST: 'http://localhost:8080',
+  },
+  publicPath: '/RuoYi-X-Umi/',
+});
