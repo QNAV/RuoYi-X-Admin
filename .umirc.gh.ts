7fe868c64c880a0fe901f9f63b112197efd64a87
Sushi Su <s.xiuxing@foxmail.com>
2022-06-12 16:55:31 +0800

    ci(gh-pages): spa

--- .umirc.gh.ts
+++ .umirc.gh.ts
@@ -2,9 +2,18 @@ import { defineConfig } from '@umijs/max';
 
 export default defineConfig({
   define: {
-    LOGIN_PATH_NAME: '/RuoYi-X-Umi/login',
     TOKEN_KEY: 'RUOYI_TOKEN',
     API_HOST: 'http://localhost:8080',
   },
   publicPath: '/RuoYi-X-Umi/',
+  base: '/RuoYi-X-Umi/',
+  headScripts: [
+    `(function () {
+  let redirect = sessionStorage.redirect;
+  delete sessionStorage.redirect;
+  if (redirect && redirect !== location.href) {
+    history.replaceState(null, null, redirect);
+  }
+})();`,
+  ],
 });
