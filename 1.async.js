8a7b6ff7c416ffde6003c1dffab1be8da598fa05
sushi-su <sushi-su@users.noreply.github.com>
2022-06-22 13:05:00 +0000

    chore(access): 清理自定义鉴权代码，使用 umi 推荐方式做鉴权管理

--- 1.async.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[334],{18185:function(t,u,n){n.r(u);var e=n(89297),_=n(11527),a=function(){return(0,_.jsx)(e._z,{children:"TODO"})};u.default=a}}]);
--- 24.async.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[436],{73849:function(c,u,n){n.r(u),n.d(u,{default:function(){return s}});var t=n(11527);function s(){return(0,t.jsx)("h1",{children:"TODO"})}}}]);
