3ea24c68879238baca7e7472a0a374c7546986f2
Sushi Su <s.xiuxing@foxmail.com>
2022-06-26 18:44:47 +0800

    improve: layout 背景色

--- src/app.tsx
+++ src/app.tsx
@@ -65,6 +65,26 @@ export const layout = ({ initialState }: { initialState: InitialState }): ProLay
       title: userInfo.user.nickName,
     },
     rightContentRender: false,
+    layoutBgImgList: [
+      {
+        src: 'https://img.alicdn.com/imgextra/i2/O1CN01O4etvp1DvpFLKfuWq_!!6000000000279-2-tps-609-606.png',
+        left: 85,
+        bottom: 100,
+        height: '303px',
+      },
+      {
+        src: 'https://img.alicdn.com/imgextra/i2/O1CN01O4etvp1DvpFLKfuWq_!!6000000000279-2-tps-609-606.png',
+        bottom: -68,
+        right: -45,
+        height: '303px',
+      },
+      {
+        src: 'https://img.alicdn.com/imgextra/i3/O1CN018NxReL1shX85Yz6Cx_!!6000000005798-2-tps-884-496.png',
+        bottom: 0,
+        left: 0,
+        width: '331px',
+      },
+    ],
     onMenuHeaderClick: () => history.push('/'),
     actionsRender: () => [<SettingsIcon key="LogoutIcon" />, <LogoutIcon key="LogoutIcon" />],
     menuDataRender: () => convertUserRoutesToMenus(userRoutes),
