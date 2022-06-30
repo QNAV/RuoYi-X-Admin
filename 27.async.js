ab79b05779b898ffb87d972b105459f9d1e4701a
sushi-su <sushi-su@users.noreply.github.com>
2022-06-30 14:53:38 +0000

    improve: 把自定义请求函数替换为插件生成的函数

--- 27.async.js
+++ 27.async.js
@@ -1,1 +1,1 @@
-"use strict";(self.webpackChunkruoyi_x_umi=self.webpackChunkruoyi_x_umi||[]).push([[362],{12517:function(t,_,u){u.r(_);var n=u(35187),e=u(11527),a=function(){return(0,e.jsx)(n._z,{children:"TODO"})};_.default=a}}]);
+"use strict";(self.webpackChunkruoyi_x_umi=self.webpackChunkruoyi_x_umi||[]).push([[362],{12517:function(t,_,u){u.r(_);var n=u(73798),e=u(11527),a=function(){return(0,e.jsx)(n._z,{children:"TODO"})};_.default=a}}]);
