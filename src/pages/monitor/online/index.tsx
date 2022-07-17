f0aaf8b61e8845bc7e56c0c2b14ce1a6deae6c8d
Sushi Su <s.xiuxing@foxmail.com>
2022-07-17 11:25:05 +0800

    feat: 用户管理

--- src/pages/monitor/online/index.tsx
+++ src/pages/monitor/online/index.tsx
@@ -1,3 +1,85 @@
-export default function () {
-  return <h1>TODO</h1>;
-}
+import { SysUserOnlineGetList, SysUserOnlinePostForceLogout } from '@/services/sys/SysUserOnlineService';
+import type { ProColumns } from '@ant-design/pro-components';
+import { ProTable } from '@ant-design/pro-components';
+import { Button, message } from 'antd';
+import type { FC } from 'react';
+
+const columns: ProColumns[] = [
+  {
+    title: '用户名称',
+    dataIndex: 'userName',
+    valueType: 'text',
+  },
+  {
+    title: '部门名称',
+    dataIndex: 'deptName',
+    valueType: 'text',
+    hideInSearch: true,
+  },
+  {
+    title: '登录IP地址',
+    dataIndex: 'ipaddr',
+    valueType: 'text',
+  },
+  {
+    title: '登录地址',
+    dataIndex: 'loginLocation',
+    valueType: 'text',
+    hideInSearch: true,
+  },
+  {
+    title: '操作系统',
+    dataIndex: 'os',
+    valueType: 'text',
+    hideInSearch: true,
+  },
+  {
+    title: '浏览器类型',
+    dataIndex: 'browser',
+    valueType: 'text',
+    hideInSearch: true,
+  },
+  {
+    title: '登录时间',
+    dataIndex: 'loginTime',
+    valueType: 'date',
+    hideInSearch: true,
+  },
+  {
+    title: '操作',
+    valueType: 'option',
+    render: (dom, entity, index, action) => {
+      return (
+        <Button
+          type="link"
+          onClick={async () => {
+            await SysUserOnlinePostForceLogout({ tokenId: entity.tokenId });
+            action?.reload();
+            message.success('强制退出成功');
+          }}
+        >
+          强退
+        </Button>
+      );
+    },
+  },
+];
+
+const OnlinePage: FC = () => {
+  return (
+    <ProTable
+      rowKey="tokenId"
+      columns={columns}
+      request={async (params) => {
+        const { rows, total } = await SysUserOnlineGetList(params as API.SysUserOnlineGetListParams);
+
+        return {
+          data: rows,
+          total,
+        };
+      }}
+    />
+  );
+};
+
+export default OnlinePage;
