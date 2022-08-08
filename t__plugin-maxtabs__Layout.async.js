730f2726deb46cacbee221464ccbf92e1d0f9f7b
sushi-su <sushi-su@users.noreply.github.com>
2022-08-08 02:58:46 +0000

    chore(deps): update

--- t__plugin-maxtabs__Layout.async.js
+++ t__plugin-maxtabs__Layout.async.js
@@ -1,1 +1,1 @@
-"use strict";(self.webpackChunkruoyi_x_umi=self.webpackChunkruoyi_x_umi||[]).push([[558],{67583:function(r,n,u){u.r(n);var _=u(60236),t=u(11527),a=function(){var e=(0,_.tE)();return(0,t.jsx)(t.Fragment,{children:e})};n.default=a}}]);
+"use strict";(self.webpackChunkruoyi_x_umi=self.webpackChunkruoyi_x_umi||[]).push([[558],{67583:function(r,n,u){u.r(n);var _=u(56323),t=u(11527),a=function(){var e=(0,_.tE)();return(0,t.jsx)(t.Fragment,{children:e})};n.default=a}}]);
