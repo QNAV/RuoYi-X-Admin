3cf74eb5361e6d082b17c4c5650c880bf33e8cd8
sushi-su <sushi-su@users.noreply.github.com>
2022-08-24 13:38:26 +0000

    chore(deps): update

--- p__demo__demo__index.async.js
+++ p__demo__demo__index.async.js
@@ -1,1 +1,1 @@
-"use strict";(self.webpackChunkruoyi_x_umi=self.webpackChunkruoyi_x_umi||[]).push([[399],{71083:function(_,n,u){u.r(n),u.d(n,{default:function(){return i}});var t=u(11527);function i(){return(0,t.jsx)("h1",{children:"TODO"})}}}]);
+"use strict";(self.webpackChunkruoyi_x_umi=self.webpackChunkruoyi_x_umi||[]).push([[399],{94258:function(_,n,u){u.r(n),u.d(n,{default:function(){return i}});var t=u(11527);function i(){return(0,t.jsx)("h1",{children:"TODO"})}}}]);
