a8ead9cafbb76e29ed6a38f3766396cb17280385
修行 <s.xiuxing@foxmail.com>
2022-09-17 17:39:11 +0800

    chore: config

--- vite.config.ts
+++ vite.config.ts
@@ -13,8 +13,17 @@ if (process.env.vis) {
   );
 }
 
+let LOGIN_PATH_NAME = '/login';
+let base = '/';
+
+if (process.env.gh) {
+  LOGIN_PATH_NAME = '/RuoYi-X-Umi/login';
+  base = '/RuoYi-X-Umi/';
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base,
   plugins,
   resolve: {
     alias: [
@@ -30,6 +39,6 @@ export default defineConfig({
     },
   },
   define: {
-    LOGIN_PATH_NAME: JSON.stringify('/login'),
+    LOGIN_PATH_NAME: JSON.stringify(LOGIN_PATH_NAME),
   },
 });
