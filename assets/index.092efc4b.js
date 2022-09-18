8aa555839b3fd6d16ad3a52d61d039a5f7bab777
sushi-su <sushi-su@users.noreply.github.com>
2022-09-18 10:41:55 +0000

    chore(deps): update

--- assets/index.092efc4b.js
+++
@@ -1,1 +0,0 @@
-import{r as E,j as r,bq as e}from"./index.05a43b4e.js";const s=t=>E.exports.cloneElement(r(e,{image:e.PRESENTED_IMAGE_SIMPLE}),t);export{s as E};
--- assets/index.b1859a36.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index.05a43b4e.js";import{B as e}from"./index.7cd247c1.js";import"./RouteContext.fab80863.js";const m=()=>r(e,{children:"TODO"});export{m as default};
