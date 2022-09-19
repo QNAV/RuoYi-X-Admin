8536d178f2f35e20b1f5f4dba3229218fb596c4d
修行 <s.xiuxing@foxmail.com>
2022-09-19 21:39:47 +0800

    fix: style

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -2,6 +2,7 @@ import { BasePageContainer } from '@/components';
 import PwdForm from '@/pages/settings/components/PwdForm';
 import UserDescriptions from '@/pages/settings/components/UserDescriptions';
 import { ProCard } from '@ant-design/pro-components';
+import { Tabs } from 'antd';
 import type { FC } from 'react';
 import { useState } from 'react';
 
@@ -19,10 +20,22 @@ const SettingsPage: FC = () => {
           <UserDescriptions />
         </ProCard>
 
-        <ProCard tabs={{ activeKey, onChange: (e) => setActiveKey(e as TabKey) }}>
-          <ProCard.TabPane key={TabKey.PASSWORD} tab="安全设置">
-            <PwdForm />
-          </ProCard.TabPane>
+        <ProCard
+          bodyStyle={{
+            paddingTop: 0,
+          }}
+        >
+          <Tabs
+            activeKey={activeKey}
+            onChange={(e) => setActiveKey(e as TabKey)}
+            items={[
+              {
+                label: '安全设置',
+                key: TabKey.PASSWORD,
+                children: <PwdForm />,
+              },
+            ]}
+          />
         </ProCard>
       </ProCard>
     </BasePageContainer>
