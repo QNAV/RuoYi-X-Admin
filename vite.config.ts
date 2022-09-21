c624b8d3a79f7bba67afabe95a9eea3e7a511e39
sushi-su <s.xiuxing@foxmail.com>
2022-09-21 09:41:45 +0800

    chore: config

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,34 @@ import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
 import type { UserConfig } from 'vite';
 import { defineConfig, loadEnv } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 const plugins: UserConfig['plugins'] = [react()];
 
-if (process.env.vis) {
-  plugins.push(
-    visualizer({
-      open: true,
-      gzipSize: true,
-    }),
-  );
-}
+const pluginVis = visualizer({
+  open: true,
+  gzipSize: true,
+});
+
+const genPluginHTML = (injectGhScript: boolean) =>
+  createHtmlPlugin({
+    minify: false,
+    inject: {
+      data: {
+        injectScript: injectGhScript ? `<script src="/404.js"></script>` : '',
+      },
+    },
+  });
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
+
+  plugins.push(genPluginHTML(mode === 'gh'));
+
+  if (process.env.vis) {
+    plugins.push(pluginVis);
+  }
 
   return {
     base: env.VITE_BASE_NAME,
