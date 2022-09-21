886768e304c8fd561baea7e32682f67e23e11b80
sushi-su <sushi-su@users.noreply.github.com>
2022-09-21 12:43:23 +0000

    refactor: 岗位管理

--- assets/index.1d6f4c97.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./index.d097f68a.js";import{B as e}from"./index.cbc4820e.js";const t=()=>a(e,{children:"TODO"});export{t as default};
--- assets/index.79e7a6d3.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./index.d097f68a.js";function n(){return r("h1",{children:"TODO"})}export{n as default};
