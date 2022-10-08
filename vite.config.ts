3f6d21c0cb7f6a4d305cf272d78cf88eb4e90233
sushi-su <s.xiuxing@foxmail.com>
2022-10-08 09:28:40 +0800

    fix: type

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import react from '@vitejs/plugin-react';
+import jotaiDebugLabel from 'jotai/babel/plugin-debug-label';
+import jotaiReactRefresh from 'jotai/babel/plugin-react-refresh';
 import { visualizer } from 'rollup-plugin-visualizer';
 import type { UserConfig } from 'vite';
 import { defineConfig, loadEnv } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
 
-const plugins: UserConfig['plugins'] = [react()];
-// const plugins: UserConfig['plugins'] = [react({ babel: { plugins: [jotaiDebugLabel, jotaiReactRefresh] } })];
+const plugins: UserConfig['plugins'] = [react({ babel: { plugins: [jotaiDebugLabel, jotaiReactRefresh] } })];
 
 const pluginVis = visualizer({
   open: true,
