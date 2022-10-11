fac1569bdc39d3ffc2862033ab494a30e68c8be1
sushi-su <sushi-su@users.noreply.github.com>
2022-10-11 13:43:12 +0000

    fix: redirect

---
+++ assets/index.d8eb471f.js
@@ -0,0 +1,1 @@
+import{aw as d,V as P,j as m,ax as l,_ as r}from"./index.73864f95.js";var n=["fieldProps","min","proFieldProps","max"],F=function(o,i){var e=o.fieldProps,a=o.min,s=o.proFieldProps,p=o.max,t=P(o,n);return m(l,r({valueType:"digit",fieldProps:r({min:a,max:p},e),ref:i,filedConfig:{defaultProps:{width:"100%"}},proFieldProps:s},t))};const c=d.forwardRef(F);export{c as P};
