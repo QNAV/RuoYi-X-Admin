b935960ac01c9dd259ccb24666f0a73576ae32f7
sushi-su <sushi-su@users.noreply.github.com>
2022-10-13 13:49:25 +0000

    chore(deps): update

--- assets/index.ebe26516.js
+++
@@ -1,1 +0,0 @@
-import{aw as d,V as P,j as m,ax as l,_ as r}from"./index.d8db007b.js";var n=["fieldProps","min","proFieldProps","max"],F=function(o,i){var e=o.fieldProps,a=o.min,s=o.proFieldProps,p=o.max,t=P(o,n);return m(l,r({valueType:"digit",fieldProps:r({min:a,max:p},e),ref:i,filedConfig:{defaultProps:{width:"100%"}},proFieldProps:s},t))};const c=d.forwardRef(F);export{c as P};
