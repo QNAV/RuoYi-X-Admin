52cdf4dd4532a5f2c22b9681dab521b051804d01
sushi-su <sushi-su@users.noreply.github.com>
2022-10-14 13:55:47 +0000

    fix: type

---
+++ assets/index.ff73ac32.js
@@ -0,0 +1,1 @@
+import{aO as d,W as P,j as m,aP as l,_ as r}from"./index.43a5fefc.js";var n=["fieldProps","min","proFieldProps","max"],F=function(o,i){var e=o.fieldProps,a=o.min,s=o.proFieldProps,p=o.max,t=P(o,n);return m(l,r({valueType:"digit",fieldProps:r({min:a,max:p},e),ref:i,filedConfig:{defaultProps:{width:"100%"}},proFieldProps:s},t))};const c=d.forwardRef(F);export{c as P};
