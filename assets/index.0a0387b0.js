7bd4bc9451b9b9c75f920535d98c1d58d2c1c150
sushi-su <sushi-su@users.noreply.github.com>
2022-10-14 02:40:38 +0000

    feat: 参数设置

---
+++ assets/index.0a0387b0.js
@@ -0,0 +1,1 @@
+import{aL as p,V as t,j as P,aM as d,_ as i}from"./index.4504b759.js";var l=["fieldProps","proFieldProps"],F=function(r,e){var o=r.fieldProps,a=r.proFieldProps,s=t(r,l);return P(d,i({ref:e,valueType:"textarea",fieldProps:o,proFieldProps:a},s))};const c=p.forwardRef(F);export{c as P};
