d5eee1f5efd9b59fd96061ad07cabc187cd6a871
sushi-su <s.xiuxing@foxmail.com>
2022-10-18 11:14:23 +0800

    chore: api type

--- src/services/sys/SysIndexService.ts
+++ src/services/sys/SysIndexService.ts
@@ -4,7 +4,7 @@ import { request } from '@/utils';
 
 /** 访问首页，提示语 GET / */
 export async function SysIndexGetIndex(options?: { [key: string]: any }) {
-  return request<string>('/', {
+  return request<any>('/', {
     method: 'GET',
     ...(options || {}),
   });
