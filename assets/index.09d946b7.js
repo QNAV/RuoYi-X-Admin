e4a51ff8b648c304e7cf2d0bef88d4933f596d20
sushi-su <sushi-su@users.noreply.github.com>
2022-10-22 09:39:48 +0000

    refactor: oper info page

--- assets/index.09d946b7.js
+++
@@ -1,1 +0,0 @@
-import{aH as p,O as t,j as P,aI as d,_ as i}from"./index.6d0b91ec.js";var l=["fieldProps","proFieldProps"],F=function(r,e){var o=r.fieldProps,a=r.proFieldProps,s=t(r,l);return P(d,i({ref:e,valueType:"textarea",fieldProps:o,proFieldProps:a},s))};const c=p.forwardRef(F);export{c as P};
--- assets/index.1826d9e1.js
+++
@@ -1,1 +0,0 @@
-import{aH as i,O as l,j as d,aI as P,_ as m}from"./index.6d0b91ec.js";var u=["fieldProps","request","params","proFieldProps"],c=function(e,r){var o=e.fieldProps,s=e.request,a=e.params,t=e.proFieldProps,p=l(e,u);return d(P,m({valueType:"treeSelect",fieldProps:o,ref:r,request:s,params:a,filedConfig:{customLightMode:!0},proFieldProps:t},p))},v=i.forwardRef(c);export{v as W};
