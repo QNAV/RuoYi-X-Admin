2c8b4d8f3492a753e8919785556812a2b10a4fd8
sushi-su <sushi-su@users.noreply.github.com>
2022-10-22 09:59:48 +0000

    refactor: 代码优化

---
+++ assets/index.175071d8.js
@@ -0,0 +1,1 @@
+import{aG as d,O as P,j as m,aH as l,_ as r}from"./index.354f04e6.js";var n=["fieldProps","min","proFieldProps","max"],F=function(o,i){var e=o.fieldProps,a=o.min,s=o.proFieldProps,p=o.max,t=P(o,n);return m(l,r({valueType:"digit",fieldProps:r({min:a,max:p},e),ref:i,filedConfig:{defaultProps:{width:"100%"}},proFieldProps:s},t))};const c=d.forwardRef(F);export{c as P};
---
+++ assets/index.1b15121a.js
@@ -0,0 +1,1 @@
+import{aG as i,O as l,j as d,aH as P,_ as m}from"./index.354f04e6.js";var u=["fieldProps","request","params","proFieldProps"],c=function(e,r){var o=e.fieldProps,s=e.request,a=e.params,t=e.proFieldProps,p=l(e,u);return d(P,m({valueType:"treeSelect",fieldProps:o,ref:r,request:s,params:a,filedConfig:{customLightMode:!0},proFieldProps:t},p))},v=i.forwardRef(c);export{v as W};
