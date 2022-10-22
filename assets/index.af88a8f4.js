b1d773e58a1915812b75fb1c870197fd7b3bfdcc
sushi-su <sushi-su@users.noreply.github.com>
2022-10-22 09:15:50 +0000

    refactor: 代码优化

---
+++ assets/index.af88a8f4.js
@@ -0,0 +1,1 @@
+import{aH as d,O as P,j as m,aI as l,_ as r}from"./index.4997ceb8.js";var n=["fieldProps","min","proFieldProps","max"],F=function(o,i){var e=o.fieldProps,a=o.min,s=o.proFieldProps,p=o.max,t=P(o,n);return m(l,r({valueType:"digit",fieldProps:r({min:a,max:p},e),ref:i,filedConfig:{defaultProps:{width:"100%"}},proFieldProps:s},t))};const c=d.forwardRef(F);export{c as P};
