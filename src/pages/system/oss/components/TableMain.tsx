d5587aefc0c5a95bf92bbd85426e5ffcc603ed17
sushi-su <s.xiuxing@foxmail.com>
2022-10-25 19:49:01 +0800

    feat: oss 列表查询

--- src/pages/system/oss/components/TableMain.tsx
+++ src/pages/system/oss/components/TableMain.tsx
@@ -2,12 +2,68 @@ import { BaseProTable } from '@/components';
 import { useActionRefMainTable } from '@/pages/system/oss/model';
 import { SysOssPostList } from '@/services/sys/SysOssService';
 import { convertParams } from '@/utils';
+import type { ProColumns } from '@ant-design/pro-components';
 import type { FC } from 'react';
+
+const columns: ProColumns<API.SysOssVo>[] = [
+  {
+    title: '对象存储主键',
+    dataIndex: 'ossId',
+    key: 'ossId',
+    valueType: 'text',
+    hideInSearch: true,
+  },
+  {
+    title: '文件名',
+    dataIndex: 'fileName',
+    key: 'fileName',
+    valueType: 'text',
+  },
+  {
+    title: '原名',
+    dataIndex: 'originalName',
+    key: 'originalName',
+    valueType: 'text',
+  },
+  {
+    title: '文件后缀名',
+    dataIndex: 'fileSuffix',
+    key: 'fileSuffix',
+    valueType: 'text',
+  },
+  {
+    title: '服务商',
+    dataIndex: 'service',
+    key: 'service',
+    valueType: 'text',
+  },
+  {
+    title: '创建时间',
+    dataIndex: 'createTime',
+    key: 'createTime',
+    valueType: 'dateTime',
+    hideInSearch: true,
+  },
+  {
+    title: '创建时间',
+    dataIndex: 'createTimeRange',
+    key: 'createTimeRange',
+    valueType: 'dateTimeRange',
+    hideInTable: true,
+  },
+];
 
 const TableMain: FC = () => {
   const actionRef = useActionRefMainTable();
 
-  return <BaseProTable actionRef={actionRef} request={(...p) => SysOssPostList(convertParams(...p))} />;
+  return (
+    <BaseProTable<API.SysOssVo, API.SysOssPageQueryBo>
+      rowKey="ossId"
+      actionRef={actionRef}
+      columns={columns}
+      request={(...p) => SysOssPostList(convertParams(...p))}
+    />
+  );
 };
 
 export default TableMain;
