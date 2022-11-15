52a031c7540b0fb5c68e120172e86b0ee06f4e42
sushi-su <s.xiuxing@foxmail.com>
2022-11-15 11:59:55 +0800

    chore: config

--- openapi.config.ts
+++ openapi.config.ts
@@ -6,11 +6,10 @@ const outputDir = path.resolve(cwd, './src/services/system');
 const templatesDir = path.resolve(cwd, './templates');
 
 generateApi({
-  url: 'https://test-ryx-admin-api.p.quicknav.cn/v3/api-docs?group=2.%E7%B3%BB%E7%BB%9F%E6%A8%A1%E5%9D%97',
+  url: 'https://test-imp-admin-api.p.quicknav.cn/v3/api-docs?group=1.%E7%B3%BB%E7%BB%9F%E6%A8%A1%E5%9D%97',
   output: outputDir,
   templates: templatesDir,
   modular: true,
   cleanOutput: true,
   httpClientType: 'axios',
-  singleHttpClient: true,
 }).catch(console.error);
