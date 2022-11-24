3e036fe9c1b2f2f0c920b9d09d5945ce5a7dbc42
sushi-su <sushi-su@users.noreply.github.com>
2022-11-24 10:57:19 +0000

    chore: config

--- assets/index-9fd71c73.js
+++
@@ -1,1 +0,0 @@
-import{d as a,au as s,a9 as o}from"./index-4fc5cf0b.js";globalThis.jotaiAtomCache=globalThis.jotaiAtomCache||{cache:new Map,get(t,e){return this.cache.has(t)?this.cache.get(t):(this.cache.set(t,e),e)}};const h=()=>a(s,{status:"404",title:"404",subTitle:"抱歉，您访问的页面不存在。",extra:a(o,{to:"/",reloadDocument:!0,children:"返回首页"})});export{h as default};
