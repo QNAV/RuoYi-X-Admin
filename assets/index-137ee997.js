4fabaf7b2961d48de2e2795b2167c222afbbb20f
sushi-su <sushi-su@users.noreply.github.com>
2022-11-24 10:44:26 +0000

    chore: config

--- assets/index-137ee997.js
+++
@@ -1,1 +0,0 @@
-import{d as a,au as r,a9 as s}from"./index-38d06ced.js";globalThis.jotaiAtomCache=globalThis.jotaiAtomCache||{cache:new Map,get(t,e){return this.cache.has(t)?this.cache.get(t):(this.cache.set(t,e),e)}};const c=()=>a(r,{status:"500",title:"500",subTitle:"抱歉，服务器出错了",extra:a(s,{to:"/",reloadDocument:!0,children:"返回首页"})});export{c as PageError,c as default};
