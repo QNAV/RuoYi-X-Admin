d3585a31fc9b5ee4c42251211765fc70f003a165
sushi-su <sushi-su@users.noreply.github.com>
2022-11-28 09:45:59 +0000

    refactor: 字典管理

--- assets/index-d0597d88.js
+++
@@ -1,1 +0,0 @@
-import{d as e,av as i,au as s,aw as c}from"./index-20700433.js";globalThis.jotaiAtomCache=globalThis.jotaiAtomCache||{cache:new Map,get(a,t){return this.cache.has(a)?this.cache.get(a):(this.cache.set(a,t),t)}};const o=()=>e(i,{children:e(s,{icon:e(c,{}),title:"Hi，欢迎回来!"})});export{o as default};
