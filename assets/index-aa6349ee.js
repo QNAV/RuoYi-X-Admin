c174032665eb436262c21389cb016d18f77b12f3
sushi-su <sushi-su@users.noreply.github.com>
2022-12-02 08:12:28 +0000

    refactor: 角色管理

---
+++ assets/index-aa6349ee.js
@@ -0,0 +1,1 @@
+import{d as a,ar as r,a6 as s}from"./index-50e1fd67.js";globalThis.jotaiAtomCache=globalThis.jotaiAtomCache||{cache:new Map,get(t,e){return this.cache.has(t)?this.cache.get(t):(this.cache.set(t,e),e)}};const c=()=>a(r,{status:"500",title:"500",subTitle:"抱歉，服务器出错了",extra:a(s,{to:"/",reloadDocument:!0,children:"返回首页"})});export{c as PageError,c as default};
