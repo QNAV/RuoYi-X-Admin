2e9bce852b85a3067d10e39ee54a3a53bb24de2a
sushi-su <sushi-su@users.noreply.github.com>
2022-12-07 08:54:21 +0000

    chore(deps): update

---
+++ assets/index-42573bf0.js
@@ -0,0 +1,1 @@
+import{d as e,au as i,av as s,aw as c}from"./index-c5acd1dc.js";globalThis.jotaiAtomCache=globalThis.jotaiAtomCache||{cache:new Map,get(a,t){return this.cache.has(a)?this.cache.get(a):(this.cache.set(a,t),t)}};const o=()=>e(i,{children:e(s,{icon:e(c,{}),title:"Hi，欢迎回来!"})});export{o as default};
