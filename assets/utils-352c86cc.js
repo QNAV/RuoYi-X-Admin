46fb1a0b38a53bb512486879bae1a37538f48e27
sushi-su <sushi-su@users.noreply.github.com>
2022-12-15 02:51:03 +0000

    refactor: keepalive

--- assets/utils-352c86cc.js
+++
@@ -1,1 +0,0 @@
-import{bI as m,cD as u}from"./index-dc2e9f21.js";import{r}from"./jsx-runtime-5957e344.js";const a=Symbol();function A(t){const o=m(t,(n,s,e)=>{const c=typeof e=="function"?e(n(o)):e;s(o,c===a?t:c)});return o}const x="w";function C(t,o){const n=u(o),s=r.exports.useContext(n).s;return r.exports.useCallback(()=>s[x](t,a),[s,t])}m(()=>[]);export{A as a,C as u};
