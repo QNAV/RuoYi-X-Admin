8c2d000cb90829c0fbfe0eb80c1da58abaa1af64
sushi <s.xiuxing@foxmail.com>
2022-12-19 15:52:15 +0800

    chore: hide loading spinner

--- src/components/LoadingProgress/index.tsx
+++ src/components/LoadingProgress/index.tsx
@@ -1,6 +1,8 @@
-import nProgress from 'nprogress';
+import NProgress from 'nprogress';
 import type { FC } from 'react';
 import { useEffect } from 'react';
+
+const nProgress = NProgress.configure({ showSpinner: false });
 
 export const LoadingProgress: FC = () => {
   useEffect(() => {
