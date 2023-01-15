5b5f7ba6cc8ab4d77959489aab24998df59ba5ee
sushi <s.xiuxing@foxmail.com>
2023-01-15 11:52:34 +0800

    chore: config

--- src/components/RouteError/index.tsx
+++ src/components/RouteError/index.tsx
@@ -1,35 +1,13 @@
-import { Result } from 'antd';
+import { Error, NotExist } from '@/components';
 import type { FC } from 'react';
-import { Link, useRouteError } from 'react-router-dom';
+import { useRouteError } from 'react-router-dom';
 
 export const RouteError: FC = () => {
   const e = useRouteError() as { status?: number; statusText?: string };
 
   if (e?.status === 404) {
-    return (
-      <Result
-        status="404"
-        title="404"
-        subTitle="抱歉，您访问的页面不存在。"
-        extra={
-          <Link to="/" reloadDocument>
-            返回首页
-          </Link>
-        }
-      />
-    );
+    return <NotExist />;
   }
 
-  return (
-    <Result
-      status="500"
-      title="500"
-      subTitle="抱歉，服务器出错了"
-      extra={
-        <Link to="/" reloadDocument>
-          返回首页
-        </Link>
-      }
-    />
-  );
+  return <Error />;
 };
