0344b3be4c80be94da96da3466d7b4882e389c76
Sushi <s.xiuxing@foxmail.com>
2023-07-20 15:37:43 +0800

    chore: 更新接口定义

--- openapi.config.ts
+++ openapi.config.ts
@@ -1,11 +1,27 @@
 import * as dotenv from 'dotenv';
-import path from 'path';
+import * as path from 'path';
 import * as process from 'process';
+import type { GenerateApiParams } from 'swagger-typescript-api';
 import { generateApi } from 'swagger-typescript-api';
+
 const cwd = process.cwd();
 const templatesDir = path.resolve(cwd, './templates');
 
 const envPath = ['.env.local', '.env'];
+
+const commonParams: Partial<GenerateApiParams> = {
+  modular: true,
+  cleanOutput: true,
+  httpClientType: 'axios',
+  codeGenConstructs: (struct) => {
+    return {
+      Keyword: {
+        ...struct.Keyword,
+        Object: 'Record<string, any>',
+      },
+    };
+  },
+};
 
 const gen = async () => {
   let API_HOST = '';
@@ -28,18 +44,14 @@ const gen = async () => {
     url: `${API_HOST}/v3/api-docs/%E4%BB%A3%E7%A0%81%E7%94%9F%E6%88%90%E6%A8%A1%E5%9D%97`,
     output: path.resolve(cwd, './src/services/gen'),
     templates: templatesDir,
-    modular: true,
-    cleanOutput: true,
-    httpClientType: 'axios',
+    ...commonParams,
   });
 
   await generateApi({
     url: `${API_HOST}/v3/api-docs/%E7%B3%BB%E7%BB%9F%E6%A8%A1%E5%9D%97`,
     output: path.resolve(cwd, './src/services/system'),
     templates: templatesDir,
-    modular: true,
-    cleanOutput: true,
-    httpClientType: 'axios',
+    ...commonParams,
   });
 
   process.exit();
