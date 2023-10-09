38f1c6ddaa8e62689c90446f09914705de6c88a8
sushi-su <sushi-su@users.noreply.github.com>
2023-10-09 06:18:48 +0000

    fix: type

---
+++ assets/index-1837d082.js
@@ -0,0 +1,1 @@
+import{j as h}from"./antdProComponents-d97761a4.js";import"./antd-79b0e736.js";globalThis.jotaiAtomCache=globalThis.jotaiAtomCache||{cache:new Map,get(t,e){return this.cache.has(t)?this.cache.get(t):(this.cache.set(t,e),e)}};function c(){return h.jsx("h1",{children:"TODO"})}export{c as default};
